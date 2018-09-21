17a92ab1a69344d31d87c22f83879ad561587e41
Dan Florio <dflo@danflorio.com>
2018-09-21 16:56:36 -0400

    add new svg

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@
 
   var tl = new TimelineMax();
 
-  var hexes = document.querySelectorAll('.home-header__graphic > defs path');
-  console.log(hexes);
+  var hexes = document.querySelectorAll('#Layer_1-2 polygon');
 
-  tl.staggerFrom(hexes, 0.5, {
+  tl.staggerFrom(hexes, 0.7, {
     scaleX: 0,
     scaleY: 0,
     transformOrigin: "center",
-    ease: Power2.easeOut
+    ease: Bounce.easeOut
   }, 0.1);
 
 })();
